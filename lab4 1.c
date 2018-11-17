5b251b9ebaef2e25d97347d04455dd3ab8209956
Adrixen <45127945+Adrixen@users.noreply.github.com>
2018-11-17 18:26:20 +0100

    Create lab4 1.c

---
+++ lab4 1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+enum Liczby{ZERO, JEDEN, DWA, TRZY, CZTERY, PIEC=4, SZESC, SIEDEM, OSIEM};
+int main(){
+printf("%d\n", ZERO);
+printf("%d\n", JEDEN);
+printf("%d\n", DWA);
+printf("%d\n", TRZY);
+printf("%d\n", CZTERY);
+printf("%d\n", PIEC);
+printf("%d\n", SZESC);
+printf("%d\n", SIEDEM);
+printf("%d\n", OSIEM);
+return 0;
+}
